be2b0748fbe4a613128e0904c22896058fbd83d5
mask <156544634@qq.com>
2018-03-28 14:58:34 +0800

    进度；

--- js/index.js
+++ js/index.js
@@ -18,23 +18,67 @@
 				jobCashFlow:400,
 				jobKidFee:140,
 			},
+			{
+				jobName:'飞行员1',
+				jobClass:'ico_polit',
+				jobCashIn:8400,
+				jobCashOut:3400,
+				jobCashFlow:400,
+				jobKidFee:140,
+			},
+			{
+				jobName:'飞行员2',
+				jobClass:'ico_polit',
+				jobCashIn:8400,
+				jobCashOut:3400,
+				jobCashFlow:400,
+				jobKidFee:140,
+			},
 		],
 		init:function(){
+			this.domSwiper();
 			this.initSwiper();
+		},
+
+		domSwiper:function(){
+			var template = $("#jobTemplate").html();
+			var compiledHtml=this.compiler(template, {
+                data: this.jobData
+            });
+            $('#jobSwiper .swiper-wrapper').html(compiledHtml);
 		},
 		initSwiper:function(){
 			var that = this;
             var swiper = new Swiper('#jobSwiper', {
                 // threshold: 30,
                 direction: 'horizontal',
-                slidesPerView:2.5,
-                spaceBetween:20,
+                slidesPerView:2,
+                spaceBetween:17,
                 initialSlide:2,
                 centeredSlides:true,
-                loop:true,
-                pagination:$('#jobSwiper .swiper-pagination')
+                loop:false,
+                autoplay:false
             });
-		}
+		},
+		compiler: function (str, data){
+	        var compiler = arguments.callee;
+	        //如果参数str全部为组词字符
+	        //console.log /^\w*$/.test(str), "xxx"
+	        var fn;
+	        if(/^\w*$/.test(str)){
+	            fn = !compiler[str] && (compiler[str] = compiler(document.getElementById(str).innerHTML));
+	        }else{
+	            fn = new Function("obj", "var p=[];with(obj){p.push('" +
+	                str.replace(/[\r\t\n]/g, " ")
+	                    .split("<@").join("\t")
+	                    .replace(/((^|@>)[^\t]*)'/g, "$1\r")
+	                    .replace(/\t=(.*?)@>/g, "',$1,'")
+	                    .split("\t").join("');")
+	                    .split("@>").join("p.push('")
+	                    .split("\r").join("\\'")+ "');}return p.join('');");
+	        }
+	        return data ? fn(data) : fn;
+	    },
 	};
 
 	window.index=index;
